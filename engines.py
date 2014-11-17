3d223b325cdcb223a1902a9bdff520c6a79ca341
chalock <chalbert.colon1@upr.edu>
2014-11-17 17:09:44 -0400

    Update engines.py
    Added Seeker movement Engine

--- engines.py
+++ engines.py
@@ -27,3 +27,11 @@ def cosxx(actor, stats):
 def cosy(actor, stats):
     t = time.clock() - actor.sign.spawn_time
     actor.centery += stats.speed*math.sin(t*math.pi)
+
+def seeker_movement(actor, stats, hero):
+    t = time.clock() - actor.sign.spawn_time
+    dx, dy = actor.centerx - hero.centerx, hero.centery - actor.centery
+    dist = math.hypot(dx,dy)
+    dx, dy = dx / dist, dy / dist
+    actor.centerx += (stats.speed * dx)
+    actor.centery += (stats.speed * dy)
